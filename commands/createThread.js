346560d2a50e3328a61e7df6aeadfea6801f706b
Rutabagarre <84787988+Rutabagarre@users.noreply.github.com>
2022-11-23 02:33:34 +0100

    create a thread with a / command

--- commands/createThread.js
+++ commands/createThread.js
@@ -1,22 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, messageLink } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
     .setName('question')
-	.setDescription('Replies with your input!')
+	.setDescription('Vous avez une question à poser à un professeur ? Allez-y !')
 	.addStringOption(option =>
-		option.setName('test')
-			.setDescription('desc')),
+		option.setName('questionnement')
+			.setDescription("Posez votre question et un channel sera créé afin d'en parler avec un professeur.")),
     async execute(interaction) {
-		const reply = interaction.options.getString('test');
-        const thread = await channel.threads.create({
-            name: 'reply',
+		const reply = interaction.options.getString('questionnement');
+        const thread = await interaction.channel.threads.create({
+            name: reply,
             autoArchiveDuration: 60,
-            reason: 'Needed a separate thread for food',
         });
-        
-        console.log(`Created thread: ${thread.name}`);
+        await thread.send({content : `<@&1044351028847460369>`})
 
-        await interaction.reply(thread);
+        await interaction.reply({content : 'Votre question a été posée dans le salon suivant :', ephemeral : true})
 	},
 };
